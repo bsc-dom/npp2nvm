67eba6f421be3a2d864a4276ab17cca329d3c0c0
Alex Barcelo <alex@betarho.net>
2020-03-27 10:25:12 +0100

    adding README.md as long_description in PyPI

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
+long_description = open("README.md").read()
+
 setup(
     name='npP2NVM',
     version="0.2",
@@ -9,6 +11,8 @@ setup(
     author="Alex Barcelo",
     author_email="alex.barcelo@bsc.es",
     description='NumPy Persistence to Non-Volatile Memory',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=["npp2nvm"],
     classifiers=[
         "Development Status :: 4 - Beta",
