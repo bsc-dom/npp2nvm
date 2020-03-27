8a145f277ac25ce90ecc520cdc3c8c4f1a7fb1b0
Alex Barcelo <alex@betarho.net>
2020-03-27 10:16:01 +0100

    bumping version, and finishing some extra metadata on setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup
 
-setup(name='npP2NVM',
-      version="0.1",
-      install_requires=[
-          "pynvm",
-      ],
-      description='NumPy Persistence to Non-Volatile Memory',
-      packages=["npp2nvm"],
-      )
+setup(
+    name='npP2NVM',
+    version="0.2",
+    install_requires=[
+        "pynvm",
+    ],
+    author="Alex Barcelo",
+    author_email="alex.barcelo@bsc.es",
+    description='NumPy Persistence to Non-Volatile Memory',
+    packages=["npp2nvm"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+    ],
+    url="https://github.com/bsc-dom/npp2nvm",
+    keywords="pynvm nvm persistence numpy",
+)
